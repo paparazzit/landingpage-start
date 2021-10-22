5455c095d7d7e42b70066652e6923355f7c8b352
paparazzit <paparazzit@fgmail.com>
2021-10-22 18:09:39 +0200

    Starter fajl

---
+++ main.js
@@ -0,0 +1,12 @@
+// BUTTONS
+let onamaBtn = document.querySelector("#oNama");
+let radioniceBtn = document.querySelector("#radionice");
+let lokacijaBtn = document.querySelector("#lokacija");
+
+// VIEWS
+
+let onamaView = document.querySelector("#onamaSek");
+let radioniceView = document.querySelector("#radioniceSek");
+let lokacijaView = document.querySelector("#mapa");
+
+// EVENTS
